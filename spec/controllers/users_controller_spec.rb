3046daee2898c8a9f19797539d2342727c491944
Plotin <git@sschoenwiese.de>
2011-02-25 17:46:32 +0100

    Finished signup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,15 +3,55 @@ require 'spec_helper'
 describe UsersController do
   render_views
   
+  describe "POST new" do
+    describe "failure" do
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+      end
+    
+      it "should not create a user" do
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+  end
+  
+  describe "success" do
+    before(:each) do
+      @attr = { :name => "Steffen Schoenwiese", :email => "test@plotin.de", :password => "test1", :password_confirmation => "test1" }
+    end
+    
+    it "should create a user" do
+      lambda do
+        post :create, :user => @attr
+      end.should change(User, :count)
+    end
+    
+    it "should redirect" do
+      post :create, :user => @attr
+      response.should redirect_to(user_path(assigns(:user))) 
+    end
+    
+  end
   
   describe "GET 'new'" do
     it "should be successful" do
-      get 'new'
+      get :new
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'new'
+      get :new
       response.should have_selector("title", :content => "Sign up")
     end
   end
